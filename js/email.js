f4b857c14a88b9870c02ccb9b560244213a02a9d
Anish-2002 <54304308+Anish-2002@users.noreply.github.com>
2026-03-05 14:24:31 +1100

    Update email.js

--- js/email.js
+++ js/email.js
@@ -3,7 +3,7 @@
  */
 export async function sendWelcomeEmail(userEmail, userName) {
     // Points to your local Node.js server
-    const url = 'https://hive-nectar-backend.onrender.com/send-welcome-email';
+    const url = 'https://hive-nectar-backend.onrender.com';
 
     try {
         const response = await fetch(url, {
@@ -29,4 +29,5 @@ export async function sendWelcomeEmail(userEmail, userName) {
     }
 
 }     
+
 
